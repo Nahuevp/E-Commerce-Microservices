5226e38a21117ba0b97487fec3e3101274f7eb7f
Nahuevp <nviera841@gmail.com>
2026-03-28 03:51:08 -0300

    fix: corrige puertos Kestrel para deploy en Render
    - Cambia http://*:8080 a http://+:8080 en todos los servicios
    - Actualiza startup.sh para usar puerto 8080 interno
    - Actualiza gateway para llamar a servicios en localhost:8080
    - Todos los servicios ahora escuchan en el mismo puerto interno

--- services/order-service/obj/Debug/net10.0/OrderService.AssemblyInfo.cs
+++ services/order-service/obj/Debug/net10.0/OrderService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982ea3e65716abf5f445fc62251751985753c0d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
